8f791d08f7bd7cb0cfc2f12894196ce243938f25
Gabriele22323 <Gabriele.gianrosso@gmail.com>
2026-05-12 10:04:22 +0200

    Added interactable actor and class

---
+++ Source/TowerTrap/Private/Actors/BaseInteractable.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/BaseInteractable.h"
+
+
+// Sets default values
+ABaseInteractable::ABaseInteractable()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABaseInteractable::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseInteractable::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABaseInteractable::Interact_Implementation_Implementation()
+{
+	UE_LOG(LogTemp,Error,TEXT("Missing interaction implementation"));
+}
+
---
+++ Source/TowerTrap/Public/Actors/BaseInteractable.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/Interactable.h"
+#include "BaseInteractable.generated.h"
+
+UCLASS()
+class TOWERTRAP_API ABaseInteractable : public AActor , public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaseInteractable();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION(BlueprintNativeEvent,BlueprintCallable)
+	void Interact_Implementation() override;
+};
