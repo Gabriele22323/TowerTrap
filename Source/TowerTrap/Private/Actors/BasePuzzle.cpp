138187e7c522493aa87de0a4e4ab2b68fb717cca
Elisa <elisaarata5@gmail.com>
2026-05-21 15:34:09 +0200

    Added base puzzle class with function to check if it is solved

---
+++ Source/TowerTrap/Private/Actors/BasePuzzle.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/BasePuzzle.h"
+
+bool ABasePuzzle::CheckIfIsSolved_Implementation()
+{
+	return bIsSolved;
+}
