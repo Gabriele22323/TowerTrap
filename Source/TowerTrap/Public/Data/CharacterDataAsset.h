3bada785c14dba70beedb77b520ef4eceb46557b
Gabriele22323 <167623594+HyperShadow375@users.noreply.github.com>
2026-08-25 16:31:51 +0200

    Added basic character data asset
    still work in progress

---
+++ Source/TowerTrap/Public/Data/CharacterDataAsset.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Enums/EDamageType.h"
+#include "CharacterDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERTRAP_API UCharacterDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	float MaxHealth;
+	TMap<EDamageType,float> DamageResistances; //multiplies taken damaged base on type
+	TMap<EDamageType,float> DamageMultipliers; //multiplies base damage
+	bool bFlies;
+	float BaseDamage;
+	float Range;
+	float MovementSpeed;
+	float DodgeChance;
+};
