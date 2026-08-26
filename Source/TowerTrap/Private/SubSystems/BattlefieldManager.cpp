8d9365be5aed10742926977cf7bd4b1f6a7b94c1
Gabriele22323 <Gabriele22323@users.noreply.github.com>
2026-08-26 10:05:04 +0200

    Added basic subsystem to manage enemies end points
    WIll expand later

---
+++ Source/TowerTrap/Private/SubSystems/BattlefieldManager.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SubSystems/BattlefieldManager.h"
+
+const TArray<FVector>& UBattlefieldManager::GetEndPoints() const
+{
+	return EndPoints;
+}
+
+void UBattlefieldManager::AddEndPoints(FVector NewPoint)
+{
+	EndPoints.Add(NewPoint);
+}
+
+void UBattlefieldManager::ClearEndPoints()
+{
+	EndPoints.Empty();
+}
---
+++ Source/TowerTrap/Public/SubSystems/BattlefieldManager.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "BattlefieldManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERTRAP_API UBattlefieldManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+private:
+	UPROPERTY(BlueprintGetter = GetEndPoints)
+	TArray<FVector> EndPoints;
+public:
+	UFUNCTION(BlueprintGetter)
+	const TArray<FVector>& GetEndPoints() const;
+	UFUNCTION(BlueprintCallable)
+	void AddEndPoints(FVector NewPoint);
+	UFUNCTION(BlueprintCallable)
+	void ClearEndPoints();
+};
